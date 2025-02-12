946a531dcea816275017c71a4ffc1d4eb33a8fb1
Lars van der Wijden <sral8991@gmail.com>
2025-02-12 14:18:59 +0100

    add raids(cm) kc to table

--- player.py
+++ player.py
@@ -12,29 +12,35 @@ def getPlayer(ign):
 
 def getTeam(players):
 	team  = []
-	igns = players.split(',')
+	igns = players.split(",")
 	for x in igns:
 		team.append(getPlayer(x))
 		#if APIiterations%20 == 0:
-		#	print('waiting for API rate-limit, program paused for 60s')
+		#	print("waiting for API rate-limit, program paused for 60s")
 		#	time.sleep(60)
 		#APIiterations += 1
 	return team
 
 def getOverview(team):
-	overview = PrettyTable(['RSN', 'CB', 'Tot.', 'Type'])
+	overview = PrettyTable(["RSN", "CB", "Tot.", "Type", "Raids", "RaidsCM"])
 	for p in team:
 		overview.add_row(getStats(p))
 	overview.reversesort = True
-	print(overview.get_string(sortby='Tot.'))
+	print(overview.get_string(sortby="Tot."))
 	overview.clear_rows()
 
 def getStats(player):
+	skills = player["latestSnapshot"]["data"]["skills"]
+	bosses = player["latestSnapshot"]["data"]["bosses"]
+	raids = bosses["chambers_of_xeric"]["kills"] + bosses["tombs_of_amascut"]["kills"] + bosses["theatre_of_blood"]["kills"]
+	raidsCM = bosses["chambers_of_xeric_challenge_mode"]["kills"] + bosses["tombs_of_amascut_expert"]["kills"] + bosses["theatre_of_blood_hard_mode"]["kills"]
 	stats = [
-		player.get('username'), 
-		player.get('combatLevel'),
-		player.get("latestSnapshot").get("data").get("skills").get("overall").get('level'),
-		player.get('type')
+		player["username"], 
+		player["combatLevel"],
+		skills["overall"]["level"],
+		player["type"],
+		raids, 
+		raidsCM
 		]
 	return stats
 
